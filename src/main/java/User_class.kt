6efbda17275055358477f397a05e55d116f4de22
Red Fox <ayartokoev3@gmail.com>
2021-01-22 20:39:26 +0300

    Last_Update_21.01.2021

---
+++ src/main/java/User_class.kt
@@ -0,0 +1,5 @@
+package com.example.messengerchat
+
+class User_class (val email: String, val id:String, val image_id: String, val  name: String, val password : String) {
+    constructor() : this("","","","","")
+}
